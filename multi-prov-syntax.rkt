430cf9c6f1de5eda9f862b8ec277b9eb1c0b7c20
Blake Johnson <blake.johnson@byu.edu>
2015-09-03 15:56:42 -0600

    added extra toplevels to test merging

--- multi-prov-syntax.rkt
+++ multi-prov-syntax.rkt
@@ -2,8 +2,9 @@
   (#%require (for-syntax '#%kernel))
   (#%provide (for-syntax test-func test-func-2 test-func-3))
   (begin-for-syntax
+    (define-values (y) 8)
     (define-values (test-func) (lambda (x) x)))
   (begin-for-syntax
-    (define-values (test-func-2) (lambda (x) x)))
+    (define-values (test-func-2) (lambda (x) y)))
   (begin-for-syntax
     (define-values (test-func-3) (lambda (x) x))))
--- simple-prov-syntax.rkt
+++ simple-prov-syntax.rkt
@@ -2,4 +2,7 @@
   (#%require (for-syntax '#%kernel))
   (#%provide (for-syntax test-func))
   (begin-for-syntax
-    (define-values (test-func) (lambda (x) x))))
+    (define-values (y) 8))
+  (begin-for-syntax
+    (define-values (z) 9)
+    (define-values (test-func) (lambda (x) y))))
